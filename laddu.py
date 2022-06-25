8b6d9f155cdf2bd7e6a6ac4a0629d568de521471
vishal <abc@ABCs-MacBook-Air.local>
2022-06-26 02:33:29 +0530

    password generator

--- laddu.py
+++ laddu.py
@@ -1,3 +1,10 @@
-x = "hello "
-y = "laddu"
-print(x+y)
+import random
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+uppercase = "ABCDEFGHIJKLMNOPQRRSTUVWXYZ"
+number = "0123456789"
+specialchar = "@#*&"
+
+all = lowercase + uppercase + number + specialchar
+length = 9
+password = "".join(random.sample(all,length))
+print("your password is : ", password)
